2fe6a0d425b90d033317ebd11f3b9df28d039c26
richardhenyash <richardash1@gmail.com>
2021-09-22 11:59:18 +0000

    add product offer delimiter to start and end of product offer string

--- products/helpers.py
+++ products/helpers.py
@@ -7,5 +7,5 @@ def get_product_offer_str(product_offers, delim):
     for product_offer in product_offers:
         product_offer_str = (
             product_offer_str + delim + product_offer.description_full)
-    product_offer_str = product_offer_str[len(delim):]
+    product_offer_str = product_offer_str + delim
     return product_offer_str
