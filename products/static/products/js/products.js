1615423eb1ef7fb09f6a655b71982b1a830f1b67
richardhenyash <richardash1@gmail.com>
2021-09-25 16:41:32 +0000

    add generic information modal and on click event handlers to information buttons, style information buttons

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -21,3 +21,37 @@ $('.product-rating-stars').each(function () {
         })
     }
 });
+
+// On click event handler added to product image link to build modal dialog
+$("#productInformationImgLink" ).click(function() {
+    (buildModal("#productInformationBtn", "information-modal-title", "#product-description-array", "#informationModal"));
+});
+
+// On click event handler added to product information button to build modal dialog
+$("#productInformationBtn" ).click(function() {
+    (buildModal("#productInformationBtn", "information-modal-title", "#product-description-array", "#informationModal"));
+});
+
+// On click event handler added to size information button to build modal dialog
+$("#sizeInformationBtn" ).click(function() {
+    (buildModal("#sizeInformationBtn", "information-modal-title", "#size-information-array", "#informationModal"));
+});
+
+// On click event handler added to type information button to build modal dialog
+$("#typeInformationBtn" ).click(function() {
+    (buildModal("#typeInformationBtn", "information-modal-title", "#type-information-array", "#informationModal"));
+});
+
+/**
+* [Function to build information modal from data attributes and javascript array]
+* @return {[modalTitle]}                     [Modal title, string]          
+*/
+function buildModal(btnId, titleAttribute, scriptId, modalId) {
+    let modalTitleId = modalId + "Title";
+    let modalContentId = modalId + "Content";
+    $(modalTitleId).text($(btnId).data(titleAttribute));
+    let contentArray = JSON.parse($(scriptId).text());
+    let contentHTML= "<p>" + contentArray.join("</p><p>") + "</p>"
+    $(modalContentId).html(contentHTML);
+    $(modalId).modal('show')
+}
