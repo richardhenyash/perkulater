fca9be1d7541170f8cb3b85267387e37843e1f93
richardhenyash <richardash1@gmail.com>
2021-09-27 11:28:35 +0000

    update product detail table styling

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import model_to_dict
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Category, Product, Size, Type, Price, Coffee, Offer
 from .helpers import get_product_offer_str
