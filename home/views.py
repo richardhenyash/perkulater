95fbec81b2fc55e8749ac63a88d155bcd8ae3cd6
richardhenyash <richardash1@gmail.com>
2021-09-29 11:18:17 +0000

    add categories seleciton to home app

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_list_or_404
-from products.models import Product, Offer
+from products.models import Category, Product, Offer
 from products.helpers import get_product_offer_str
 
 
@@ -7,6 +7,7 @@ def index(request):
     """ A view to return the index page """
 
     products = Product.objects.all()
+    categories_all = Category.objects.all()
     product_offers = get_list_or_404(Offer, display_in_banner=True)
     product_offer_str = get_product_offer_str(product_offers, "  -  ")
 
@@ -14,5 +15,6 @@ def index(request):
         'products': products,
         'product_offers' : product_offers,
         'product_offer_str': product_offer_str,
+        'categories_all': categories_all,
     }
     return render(request, 'home/index.html', context)
