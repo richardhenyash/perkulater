e4651fe9b3db9f29da41da1949e8457c8aca047b
richardhenyash <richardash1@gmail.com>
2021-09-30 10:29:35 +0000

    update basket context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,31 @@
+from decimal import Decimal
+from django.conf import settings
+
+
 def basket_contents(request):
 
-    context = {}
+    basket_items = []
+    total = 0
+    product_count = 0
+    free_delivery_amount = settings.FREE_DELIVERY_AMOUNT
+    delivery_percentage = settings.STANDARD_DELIVERY_PERCENTAGE
+
+    if total < free_delivery_amount:
+        delivery = total * (Decimal(delivery_percentage / 100))
+        free_delivery_delta = free_delivery_amount - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_amount': free_delivery_amount,
+        'free_delivery_delta': free_delivery_delta,
+        'grand_total': grand_total,
+    }
     return context
