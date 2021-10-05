f0aa8c50f66e471629e1483ce9b0ef19d10d4cbd
richardhenyash <richardash1@gmail.com>
2021-10-05 13:03:24 +0000

    update basket template to read contexts

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product, Price, Offer, Size
+from products.models import Category, Product, Price, Offer, Size
 
 
 def basket_contents(request):
@@ -16,18 +16,20 @@ def basket_contents(request):
         product_size = product_info_array[1]
         product_type = product_info_array[2]
         product = get_object_or_404(Product, pk=product_id)
-        print(product)
-        print(product.id)
+        product_category = get_object_or_404(Category, name=product.category)
         size = get_object_or_404(Size, size=product_size)
         queryset = Price.objects.filter(product=product_id, size=size.id)
         product_price = get_object_or_404(queryset)
+        line_item_price = product_price.price * product_quantity
         basket_items.append({
             'product': product,
             'product_id': product.id,
-            'quantity': product_quantity,
+            'product_category': product_category,
+            'product_quantity': product_quantity,
             'product_size': product_size,
             'product_type': product_type,
             'product_price': product_price.price,
+            'line_item_price': line_item_price,
         })
     print(basket_items)
     offer = get_object_or_404(Offer, description="Delivery")
