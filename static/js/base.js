74730826c57cced3747b63de70756558fdfc1f90
richardhenyash <richardash1@gmail.com>
2021-10-09 09:03:02 +0000

    update toast initialisation javascript and toast styling

--- static/js/base.js
+++ static/js/base.js
@@ -6,13 +6,18 @@
 /*jshint esversion: 6 */
 
 /* Show Toast Messages */
-$(document).ready(function(){
-    $('.toast').show();
-});
+/* Thanks to help from Shaun at code instutute, https://getbootstrap.com/docs/5.0/components/toasts/#usage */
+/* and https://stackoverflow.com/questions/63515279/how-to-initialize-toasts-with-javascript-in-bootstrap-5 */
 
-/* Hide toast Messages on click of close button */
-$('.toast-close').each(function() {
-    $(this).click(function() {
-        $('.toast').hide();
-    });
-});
+let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+let toastList = toastElList.map(function (toastEl) {
+    /* Set toast initialisation options */
+    let option = {
+        animation: true,
+        autohide: true,
+        delay: 5000,
+    }
+  let bsToast = new bootstrap.Toast(toastEl, option)
+  /* Show toasts */
+  bsToast.show();
+})
