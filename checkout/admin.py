0004a30e385bb3777dae6295a8950b3c3fef31e7
richardhenyash <richardash1@gmail.com>
2021-10-10 10:01:58 +0000

    add signals and form to checkout app, update models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInLine(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInLine,)
+
+    readonly_fields = ('order_number', 'date', 'delivery_cost',
+                       'order_total', 'grand_total')
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'address_1', 
+              'address_2', 'town_or_city', 'county',
+              'postcode', 'country', 'order_total',
+              'delivery_cost', 'grand_total',
+              'original_basket', 'stripe_pid')
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total')
+                    
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -8,6 +8,7 @@ from products.models import Offer, Price, Product, Size, Type
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -17,7 +18,6 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
