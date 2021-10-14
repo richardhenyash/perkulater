4164ad4f0cf908337ae6ab9d0ea7d8d2fd67be0c
richardhenyash <richardash1@gmail.com>
2021-10-14 17:18:02 +0000

    add and run product view tests

--- products/test_views.py
+++ products/test_views.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+
+
+class TestProductViews(TestCase):
+
+    def test_get_all_products(self):
+        response = self.client.get('/products')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/templates/products.html')
+
+    def test_get_product_details(self):
+        product = Product.objects.create(
+            name="Test Coffee")
+        response = self.client.get(f'/products/{product.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/templates/product_detail.html')
