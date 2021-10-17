c3fa301c89dc59edb9fb3da655f6c898f26f2eb7
richardhenyash <richardash1@gmail.com>
2021-10-17 11:12:34 +0000

    add POST handler to profile view, update toast success message

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserForm, UserProfileForm
@@ -11,8 +12,16 @@ def profile(request):
     """
     user_profile = get_object_or_404(UserProfile, user=request.user)
     userobj = User.objects.get(username=request.user)
-    print(userobj)
-    print(userobj.first_name)
+    
+    if request.method == 'POST':
+        user_form = UserForm(
+            request.POST, instance=userobj)
+        user_profile_form = UserProfileForm(
+            request.POST, instance=user_profile)
+        if user_profile_form.is_valid and user_form.is_valid():
+            user_form.save()
+            user_profile_form.save()
+            messages.success(request, "Profile successfully updated")
 
     user_profile_form = UserProfileForm(instance=user_profile)
     user_form = UserForm(instance=userobj)
@@ -23,5 +32,6 @@ def profile(request):
         'user_form': user_form,
         'user_profile_form': user_profile_form,
         'orders': orders,
+        'on_profile_page': True,
     }
     return render(request, template, context)
