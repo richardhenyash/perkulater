b7962d96377383b7c61812d3ce64095589334a01
richardhenyash <richardash1@gmail.com>
2021-10-17 07:29:49 +0000

    configure admin for Profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from.models import UserProfile
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+    )
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
