86a9c34f483e7fe87105ae159144c37c4003fbd0
richardhenyash <richardash1@gmail.com>
2021-10-31 13:13:44 +0000

    update unit testing data, update and run unite tests on products views

--- products/test_data.py
+++ products/test_data.py
@@ -1,11 +1,19 @@
-from .models import Category, Coffee, Offer, Price, Product, Size, Type
+from .models import Category, Coffee, Offer, Price, Product, Review, Size, Type
+from django.contrib.auth.models import User
 
 
 def build_test_data():
     """Function to buld the test database, run before unit tests"""
+    # Create admin user for unit tests
+    adminuser = User.objects.create_superuser(
+        'unittestadmin', 'unittestadmin@test.com', 'unittestadminpassword')
+    # Creaet standard user for unit tests
+    user = User.objects.create_user(
+        'unittestuser', 'unittestuser@test.com', 'unittestuserpassword')
     category = Category.objects.create(
         name="Coffee",
-        friendly_name="All of our top quality coffees",
+        friendly_name="Coffee",
+        description="Test description",
         size_description="Size",
         size_information="Test Size Information Line 1;Test Size Information Line 2;Test Size Information Line 3",
         type_description="Grind",
@@ -21,20 +29,41 @@ def build_test_data():
         description_short="Test Short Description Line 1;Test Short Description Line 2",
         description_delimiter=";",
         rating=4.50,
-        image_url="jump-leads-front-transparent.png",
         image="jump-leads-front-transparent.png",
+    )
+    Type.objects.create(
+        category=category,
+        type="Coarse"
+    )
+    Type.objects.create(
+        category=category,
+        type="Medium"
+    )
+    Type.objects.create(
+        category=category,
+        type="Fine"
     )
     Type.objects.create(
         category=category,
         type="Whole Bean"
     )
-    size = Size.objects.create(
+    size_small = Size.objects.create(
         category=category,
-        size="250g")
+        size="250g",
+        default_price=7.50)
+    size_large = Size.objects.create(
+        category=category,
+        size="1kg",
+        default_price=25.50)
     Price.objects.create(
         product=product,
-        size=size,
+        size=size_small,
         price=7.50,
+        sku="TEST SKU")
+    Price.objects.create(
+        product=product,
+        size=size_large,
+        price=25.50,
         sku="TEST SKU")
     Coffee.objects.create(
         product=product,
@@ -51,7 +80,15 @@ def build_test_data():
         description="Delivery",
         free_delivery_amount=30.00,
         delivery_minimum=2.00,
-        delivery_percentage=10.00
+        delivery_percentage=10.00,
+        display_in_banner=True
+    )
+    Offer.objects.create(
+        description="Review",
+        description_full="Add a review to get 10% off your next order",
+        free_delivery_amount=0.00,
+        discount=10.00,
+        display_in_banner=True
     )
     Offer.objects.create(
         description="Test Offer 1",
@@ -62,4 +99,16 @@ def build_test_data():
         description="Test Offer 2",
         description_full="Test Offer 2 Description Full",
         display_in_banner=True
+    )
+    Review.objects.create(
+        product=product,
+        user=adminuser,
+        rating=5,
+        review="Awesome tasting coffee - the best!"
+    )
+    Review.objects.create(
+        product=product,
+        user=user,
+        rating=1,
+        review="Not great - don't buy it!"
     )
--- products/test_views.py
+++ products/test_views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.test import TestCase
 from django.urls import resolve
-from .models import Product
+from django.contrib.auth.models import User
+from .models import Category, Coffee, Product, Price, Review, Size
 
 from .test_data import build_test_data
 
@@ -19,10 +20,48 @@ class TestProductViews(TestCase):
 
     def test_resolve_product_detail(self):
         """Test resolving product detail view"""
-        product = Product.objects.create(
-            name="Test Coffee")
+        product = get_object_or_404(Product, name="Test Coffee")
         found = resolve(f'/products/{product.id}/')
         self.assertEqual(found.url_name, "product_detail")
+
+    def test_resolve_add_product(self):
+        """Test resolving add product view"""
+        found = resolve('/products/add/')
+        self.assertEqual(found.url_name, "add_product")
+
+    def test_resolve_edit_product(self):
+        """Test resolving edit product view"""
+        product = get_object_or_404(Product, name="Test Coffee")
+        found = resolve(f'/products/edit/{product.id}/')
+        self.assertEqual(found.url_name, "edit_product")
+
+    def test_resolve_delete_product(self):
+        """Test resolving delete product view"""
+        product = get_object_or_404(Product, name="Test Coffee")
+        found = resolve(f'/products/delete/{product.id}/')
+        self.assertEqual(found.url_name, "delete_product")
+
+    def test_resolve_edit_prices(self):
+        """Test resolving editing prices for a product"""
+        product = get_object_or_404(Product, name="Test Coffee")
+        found = resolve(f'/products/edit_prices/{product.id}/')
+        self.assertEqual(found.url_name, "edit_prices")
+
+    def test_resolve_review_product(self):
+        """Test resolving product review"""
+        product = get_object_or_404(Product, name="Test Coffee")
+        found = resolve(f'/products/review_product/{product.id}/')
+        self.assertEqual(found.url_name, "review_product")
+
+    def test_resolve_delete_review(self):
+        """Test resolving delete review"""
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        print(loginresponse)
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        user = get_object_or_404(User, username='unittestuser')
+        found = resolve(f'/products/delete_review/{product.id}/{user.id}/')
+        self.assertEqual(found.url_name, "delete_review")
 
     def test_get_all_products(self):
         """Test returning all products view"""
@@ -52,4 +91,194 @@ class TestProductViews(TestCase):
         url = f'/products/{product.id}/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/product_detail.html")
+        
+    def test_get_add_product(self):
+        """Test returning add product view"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)      
+        response = self.client.get('/products/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/add_product.html")
+
+    def test_post_add_product(self):
+        """Test adding a product"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)    
+        category = get_object_or_404(Category, name="Coffee")
+        response = self.client.post(
+            '/products/add/',
+            {
+                'category': category.id,
+                'name': 'Test New Coffee',
+                'friendly_name': "Test New Coffee Friendly Name",
+                'friendly_price': "£7.50 - 250g",
+                'description_full': "Test Full Description Paragraph 1;Test Full Description Paragraph 2;Test Full Description Paragraph 3",
+                'description_short': "Test Short Description Line 1;Test Short Description Line 2",
+                'description_delimiter': ";",
+                'rating': 4.50,
+                'country': "Test Country",
+                'farm': "Test Farm",
+                'owner': "Test Owner",
+                'variety': "Test Variety",
+                'altitude': "Test Variety",
+                'town': "Test Town",
+                'region': "Test Region",
+                'flavour_profile': "Test Flavour Profile",
+            })
+        # Get sizes
+        size_small = get_object_or_404(Size, size="250g")
+        size_large = get_object_or_404(Size, size="1kg")
+        # Get new product
+        product_new = get_object_or_404(Product, name="Test New Coffee")
+        # Get new coffee
+        coffee_new = get_object_or_404(Coffee, product=product_new)
+        # Get new prices
+        price_small = get_object_or_404(Price, product=product_new, size=size_small)
+        price_large = get_object_or_404(Price, product=product_new, size=size_large)
+        # Perform tests
+        self.assertEqual(price_small.price, 7.50)
+        self.assertEqual(price_large.price, 25.50)
+        self.assertEqual(product_new.name, "Test New Coffee")
+        self.assertEqual(coffee_new.country, "Test Country")
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_edit_product(self):
+        """Test returning edit product view"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        url = f'/products/edit/{product.id}/'  
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/edit_product.html")
+
+    def test_post_edit_product(self):
+        """Test editing a product"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        category = get_object_or_404(Category, name="Coffee")
+        url = f'/products/edit/{product.id}/'
+        response = self.client.post(
+            url,
+            {
+                'category': category.id,
+                'name': 'Test Updated Coffee',
+                'friendly_name': "Test Updated Coffee Friendly Name",
+                'friendly_price': "£7.50 - 250g",
+                'description_full': "Test Full Description Paragraph 1;Test Full Description Paragraph 2;Test Full Description Paragraph 3",
+                'description_short': "Test Short Description Line 1;Test Short Description Line 2",
+                'description_delimiter': ";",
+                'rating': 4.50,
+                'country': "Test Updated Country",
+                'farm': "Test Farm",
+                'owner': "Test Owner",
+                'variety': "Test Variety",
+                'altitude': "Test Variety",
+                'town': "Test Town",
+                'region': "Test Region",
+                'flavour_profile': "Test Flavour Profile",
+            })
+        # Get updated product
+        product_updated = get_object_or_404(Product, name="Test Updated Coffee")
+        # Get updated coffee
+        coffee_updated = get_object_or_404(Coffee, product=product_updated)
+        # Perform tests
+        self.assertEqual(product_updated.name, "Test Updated Coffee")
+        self.assertEqual(product_updated.friendly_name, "Test Updated Coffee Friendly Name")
+        self.assertEqual(coffee_updated.country, "Test Updated Country")
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_product(self):
+        """Test deleting a product"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        response = self.client.post(f'/products/delete/{product.id}/')
+        product_deleted = Product.objects.filter(name="Test Coffee")
+        # Perform tests
+        self.assertEqual(len(product_deleted), 0)
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_edit_prices(self):
+        """Test returning edit prices view for a product"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        url = f'/products/edit_prices/{product.id}/'  
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/edit_prices.html")
+
+    def test_post_edit_prices(self):
+        """Test editing prices for a product"""
+        # login as admin user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        size_small = get_object_or_404(Size, size="250g")
+        url = f'/products/edit_prices/{product.id}/'
+        response = self.client.post(
+            url,
+            {
+                'size': size_small.id,
+                'price': 9.50,
+            })
+        # Get updated price
+        price_updated = get_object_or_404(Price, product=product, size=size_small)
+        # Perform tests
+        self.assertEqual(price_updated.price, 9.50)
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_review(self):
+        """Test returning review for a product"""
+        # login as standard user
+        loginresponse = self.client.login(username='unittestuser', password='unittestuserpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        url = f'/products/review_product/{product.id}/'  
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/review_product.html")
+
+    def test_post_edit_review(self):
+        """Test editing a review for a product"""
+        # login as standard user
+        loginresponse = self.client.login(username='unittestuser', password='unittestuserpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        user = get_object_or_404(User, username='unittestuser')
+        url = f'/products/review_product/{product.id}/'
+        response = self.client.post(
+            url,
+            {
+                'rating': 5,
+                'review': "Awesome coffee!",
+            })
+        # Get updated review
+        review_updated = get_object_or_404(Review, product=product, user=user)
+        # Perform tests
+        self.assertEqual(review_updated.rating, 5)
+        self.assertEqual(review_updated.review, "Awesome coffee!")
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_user_review(self):
+        """Test deleting a user review for a product"""
+        # login as standard user
+        loginresponse = self.client.login(username='unittestadmin', password='unittestadminpassword')
+        self.assertTrue(loginresponse)
+        product = get_object_or_404(Product, name="Test Coffee")
+        user = get_object_or_404(User, username='unittestuser')
+        url = f'/products/delete_review/{product.id}/{user.id}/'
+        response = self.client.post(url)
+        # Perform tests
+        review_deleted = Review.objects.filter(product=product, user=user)
+        # Perform tests
+        self.assertEqual(len(review_deleted), 0)
+        self.assertEqual(response.status_code, 302)
