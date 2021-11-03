cc2897fbb0d815f0ab9a28f91020e575e482ee9c
richardhenyash <richardash1@gmail.com>
2021-11-03 09:23:26 +0000

    move confirm and onformation modals to project root directory, move associated javascript to base.js

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -4,7 +4,7 @@
 * @version 1.1.1
 */
 /*jshint esversion: 6 */
-/* globals $ */
+/* globals $, buildConfirmModal, buildInformationModal */
 
 /* Set product rating stars for all divs with the class product-rating-stars */
 /* Note - reads the product rating from the product-rating data attribute */
@@ -124,71 +124,6 @@ function colourStars(rating, starIdPrefix) {
     }
     $('input[name=rating]').val(rating);
     return rating;
-}
-
-/**
-* [Function to build information modal from data attributes and javascript content array]
-* @return {[modalTitle]}                     [Modal title, string]          
-*/
-function buildInformationModal(btnId, titleAttribute, scriptId, modalId, modalSize) {
-    let modalTitleId = modalId + "Title";
-    let modalContentId = modalId + "Content";
-    let modalSizeId = modalId + "Size";
-    // Get modal title from data atttribute
-    let modalTitle = $(btnId).data(titleAttribute);
-    $(modalTitleId).text(modalTitle);
-    let contentHTML;
-    if (scriptId) {
-        // Get modal content from javascript content array if passed
-        let contentArray = JSON.parse($(scriptId).text());
-        // Build content HTML
-        contentHTML= "<p>" + contentArray.join("</p><p>") + "</p>";
-
-    } else {
-        // Else get modal content from data attribute
-        let modalContent = $(btnId).data("information-modal-content");
-        // Build content HTML
-        contentHTML= "<p>" + modalContent + "</p>";
-    }
-    // Remove size classes
-    $(modalSizeId).removeClass("modal-sm");
-    $(modalSizeId).removeClass("modal-lg");
-    $(modalSizeId).removeClass("modal-xl");
-    // Set modal size
-    if ((modalSize) != "") {
-        $(modalSizeId).addClass(modalSize);
-    }
-    // Update modal content
-    $(modalContentId).html(contentHTML);
-    // Show content
-    $(modalId).modal('show');
-    return modalTitle;
-}
-
-/**
-* [Function to build confirm modal from data attributes]
-* @return {[modalTitle]}                     [Modal title, string]          
-*/
-function buildConfirmModal(btnId, modalId) {
-    let modalTitleId = modalId + "Title";
-    let modalContentId = modalId + "Content";
-    let modalLinkId = modalId + "Confirmed";
-    // Get modal title from data atttribute
-    let modalTitle = $(btnId).data("confirm-modal-title");
-    $(modalTitleId).text(modalTitle);
-    // Get modal content from data attribute
-    let modalContent = $(btnId).data("confirm-modal-content");
-    // Build content HTML
-    let contentHTML= "<p>" + modalContent + "</p>";
-    // Update modal content
-    $(modalContentId).html(contentHTML);
-    // Get modal link from data atttribute
-    let modalLink = $(btnId).data("confirm-modal-link");
-    // Update modal link
-    $(modalLinkId).attr("href", modalLink);
-    // Show content
-    $(modalId).modal('show');
-    return modalTitle;
 }
 
 /**
